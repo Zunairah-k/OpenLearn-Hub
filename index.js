029a72effc7cccc2c40ef42ba84b6c3414b2b2b7
Syeda Umaima <syedaumaima07@gmail.com>
2025-03-15 22:07:59 +0530

    modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector("nav ul");
+    const navMenu = document.querySelector(".nav-links");
 
     menuToggle.addEventListener("click", function () {
         navMenu.classList.toggle("show"); // Toggle 'show' class to open/close menu
