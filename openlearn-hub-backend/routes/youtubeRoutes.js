75c3ad202886257a8c8ddb8f69dedef1e33520f8
Syeda Umaima <syedaumaima07@gmail.com>
2025-05-30 02:03:44 +0530

    deleted backend files from openlearn-hub-backend

--- openlearn-hub-backend/routes/youtubeRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { searchCourses } = require("../controllers/youtubeController");
-
-router.get("/search", searchCourses);
-
-module.exports = router;
